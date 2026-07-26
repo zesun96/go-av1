1c8c058857f88ac5266173fc38be568444a53afc
zesun <sunze1@foxmail.com>
2026-07-26 14:09:30 +0800

    choreL bump version to  0.1.0

--- pkg/av1/version.go
+++ pkg/av1/version.go
@@ -1,7 +1,4 @@
 package av1
 
 // Version reports the semantic version of go-av1.
-//
-// During the M0 scaffold the version is fixed at "0.0.0-m0". It will be
-// bumped at each milestone tag.
-const Version = "0.0.0-m0"
+const Version = "0.1.0"
